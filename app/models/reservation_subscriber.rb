770eaa2590259df73f4777cc8a975b1a5fc9bb79
Roman <roman.simecek@screenconcept.ch>
2009-07-26 04:09:56 +0200

    expanded the validation of reservation_subscriber model

--- app/models/reservation_subscriber.rb
+++ app/models/reservation_subscriber.rb
@@ -4,5 +4,6 @@ class ReservationSubscriber < ActiveRecord::Base
   validates_presence_of :email
   validates_format_of :email,
                       :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+  has_many :reservations
 
 end
