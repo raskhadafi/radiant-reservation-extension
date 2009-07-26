7e8d35093e5c4d5a0e18f0d2b24a45995879b63d
Roman <roman.simecek@screenconcept.ch>
2009-07-26 05:43:49 +0200

    admin area works fine now

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -12,11 +12,23 @@ class Admin::ReservationsController < ApplicationController
     @reservation_item = ReservationItem.find(params[:id])
     render(:action => 'edit')
   end
+  
+  def edit
+    @reservation = Reservation.find(params[:id])
+    @reservation_item = ReservationItem.find(@reservation.reservation_item_id)
+    @reservation_subscriber = ReservationSubscriber.find(:all)
+    render(:action => 'edit')
+  end
 
   def create
+    @reservation_item = ReservationItem.find(params[:reservation][:reservation_item])
+    params[:reservation][:reservation_item] = @reservation_item
+    @reservation_subscriber = ReservationSubscriber.find(:all)
+    params[:reservation][:reservation_subscriber] = ReservationSubscriber.find(params[:reservation][:reservation_subscriber])
+    params[:reservation][:from]
     @reservation = Reservation.new(params[:reservation])
     if @reservation.save
-      redirect_to(:back)
+      redirect_to(edit_admin_reservation_item_url(@reservation_item))
     else
       render(:action => 'edit')
     end
@@ -24,8 +36,11 @@ class Admin::ReservationsController < ApplicationController
 
   def update
     @reservation = Reservation.find(params[:id])
+    @reservation_item = ReservationItem.find(params[:reservation][:reservation_item])
+    params[:reservation][:reservation_item] = @reservation_item
+    params[:reservation][:reservation_subscriber] = ReservationSubscriber.find(params[:reservation][:reservation_subscriber])
     if @reservation.update_attributes(params[:reservation])
-      redirect_to(:back)
+      redirect_to(edit_admin_reservation_item_url(@reservation_item))
     else
       render(:action => 'edit')
     end
--- app/models/reservation_subscriber.rb
+++ app/models/reservation_subscriber.rb
@@ -5,5 +5,5 @@ class ReservationSubscriber < ActiveRecord::Base
   validates_format_of :email,
                       :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
   has_many :reservations
-
+  
 end
