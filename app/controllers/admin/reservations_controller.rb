590442c261efa23bbbf1bca6bf885d27431bde5a
Roman <roman.simecek@screenconcept.ch>
2009-07-26 04:03:01 +0200

    new reserations controller

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -8,7 +8,34 @@ class Admin::ReservationsController < ApplicationController
 
   def new
     @reservation = Reservation.new
+    @reservation_subscriber = ReservationSubscriber.find(:all)
+    @reservation_item = ReservationItem.find(params[:id])
     render(:action => 'edit')
+  end
+
+  def create
+    @reservation = Reservation.new(params[:reservation])
+    if @reservation.save
+      redirect_to(:back)
+    else
+      render(:action => 'edit')
+    end
+  end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+    if @reservation.update_attributes(params[:reservation])
+      redirect_to(:back)
+    else
+      render(:action => 'edit')
+    end
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    flash[:error] = "The reservation was deleted."
+    redirect_to(:back)
   end
 
 end
